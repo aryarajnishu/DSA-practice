b9db4c843094f9dca87b56ed1eef9e3a1c6cb7c3
aryarajnishu <22bds007@iiitdwd.ac.in>
2024-10-18 00:02:48 +0530

    new

--- 15_GRAPH_TUF/practice.cpp
+++ 15_GRAPH_TUF/practice.cpp
@@ -1,46 +1,67 @@
 #include <bits/stdc++.h>
-using namespace std;
+using namespace std; 
 
-bool isCyclicDFS(int index, unordered_map<int, bool> &visited,  unordered_map<int, bool> &restack , unordered_map<int, vector<int>> &adj, int parent) {
-    
-    return false;
-}
+void fun(vector<vector<int>>& times, int targetFriend) {
+    int n = times.size();
+    vector<vector<int>> map(n , vector<int>(2));
+    unordered_map<int , int> map1;
+    int it, ot;
+    for(int i = 0; i < times.size(); i++){
+        if(targetFriend == i){
+            it = times[i][0];
+            ot = times[i][1];
+        }
+        map[i][0] = times[i][0];
+        map[i][1] = times[i][1];
 
-int main() {
-    unordered_map<int, vector<int>> adj;
-    adj[1] = {2,5};
-    adj[2] = {3,5};
-    adj[3] = {4};
-    adj[4] = {2};
-    adj[5] = {};
-
-    unordered_map<int, bool> visited;
-    unordered_map<int , bool> restack;
-    for (int i = 1; i <= 5; i++) {
-        visited[i] = false;
-        restack[i] = false;
+        map1[times[i][0]] = times[i][1];
     }
 
-    for (int i = 1; i <= 5; i++) {
-        if (!visited[i]) {
-            bool ans = isCyclicDFS(i, visited,restack, adj, -1);
-            if (ans) {
-                cout << "Graph contains cycle" << endl;
-                return 0;
+    sort(map.begin(), map.end());
+
+    int ans;
+    int i;
+    for(i = 0; i < map.size(); i++){
+        if(map[i][0] == it) ans = i;
+        cout << map[i][0] << " " << map[i][1];
+        cout << endl;
+    }
+    cout << endl;
+    
+    unordered_map<int, int> map2;
+    for(int i = 0; i <= ans; i++){
+        for(int j = i + 1; j <= ans; j++){
+            if(map[i][1] < it && map[i][1] < map[j][0]){
+                map2[map[i][1]] = map[j][0];
+                break;
             }
         }
     }
-
-    cout << "Graph doesn't contain a cycle" << endl;
-    return 0;
+    for(auto i : map2){
+        cout << i.first << " " << i.second << endl;
+    }
+    cout << endl;
+    cout << ans << " " << it << endl;
+    int k, x, y;
+    for(k = 0; k <= ans; k++){
+        x = map[k][0];
+        y = map[k][1];
+        while(y < it){
+            if(map2.find(y) == map2.end()) break;
+            x = map2[y];
+            y = map1[x];
+            if(x == it){
+                break;
+            }
+        }
+    }
+    cout << "ans is :" << k;
 }
 
+int main(){
+    vector<vector<int>> times = {{33889,98676},{80071,89737},{44118,52565},{52992,84310},{78492,88209},{21695,67063},{84622,95452},{98048,98856},{98411,99433},{55333,56548},{65375,88566},{55011,62821},{48548,48656},{87396,94825},{55273,81868},{75629,91467}};
+    int targetFriend = 6;
 
-
-
-// unordered_map<int, vector<int>> adj;
-//     adj[1] = {2,3,4};
-//     adj[2] = {1,4};
-//     adj[3] = {1,5};
-//     adj[4] = {1,2,5};
-//     adj[5] = {3,4};
+    fun(times, targetFriend);
+    return 0;
+}
--- 16_DP_series/07_numOfDearrangement.cpp
+++ 16_DP_series/07_numOfDearrangement.cpp
@@ -17,10 +17,37 @@ int McountDerangement(int n, vector<int> &dp) {
     return dp[n];
 }
 
+int TcountDerangement(int n) {
+    vector<int> dp(n + 1);
+    dp[0] = 1;
+    dp[1] = 0;
+    dp[2] = 1;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (i - 1) * (dp[i - 1] + dp[i - 2]);
+    }
+    return dp[n];
+}
+
+int ScountDerangement(int n) {
+    if (n == 0) return 1;
+    if (n == 1) return 0;
+    int pre2 = 1;
+    int pre1 = 0;
+    int curr;
+    for (int i = 2; i <= n; i++) {
+        curr = (i - 1) * (pre1 + pre2);
+        pre2 = pre1;
+        pre1 = curr;
+    }
+    return curr;
+}
+
 int main() {
-    int n = 4;
+    int n = 5;
     vector<int> dp(n + 1, -1);
     cout << RcountDerangement(n) << endl;
     cout << McountDerangement(n, dp) << endl;
+    cout << TcountDerangement(n) << endl;
+    cout << ScountDerangement(n) << endl;
     return 0;
 }
